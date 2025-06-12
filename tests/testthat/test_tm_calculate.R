a8e2f0e377ebe1c1895257bed736af38d85f6518
JunhuiLi1017 <junhuili@cau.edu.cn>
2025-06-12 00:05:29 -0400

    update testthat

--- tests/testthat/test_tm_calculate.R
+++ tests/testthat/test_tm_calculate.R
@@ -6,5 +6,5 @@ test_that("test_tm_calculate failed", {
   
   fasta_file  <- system.file("extdata","BSgenome.Hsapiens.UCSC.hg38.fasta", package = "TmCalculator")
   gr_tm <- tm_calculate(fasta_file)
-  expect_equal(round(gr_tm$tm$Tm$Tm,5), c(67.06562,69.64434))
+  expect_equal(round(gr_tm$tm$Tm$Tm,5)[c(1,10,50,100,141)], c(76.57807,72.88846,70.04297,67.24338,75.47229))
 })
