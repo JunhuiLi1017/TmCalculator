08bae0ee0181fc4e3e3def7b60d46311030b2e40
JunhuiLi1017 <junhuili@cau.edu.cn>
2026-06-08 15:41:11 -0400

    del shiny folder

--- TmCalculatorShiny/inst/app/app.R
+++
@@ -1,4 +0,0 @@
-# Entry point for `shiny::runApp(system.file("app", package = "TmCalculatorShiny"))`
-library(TmCalculatorShiny)
-shiny::shinyApp(ui = TmCalculatorShiny:::app_ui(),
-                server = TmCalculatorShiny:::app_server)
--- TmCalculatorShiny/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(TmCalculatorShiny)
-
-test_check("TmCalculatorShiny")
